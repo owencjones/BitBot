ba2c17eae1f71de25fc272588c8bda80b1c151da
Owen Jones <ojones@sorensonmedia.com>
2017-04-10 22:43:17 +0100

    First working version -hopefully

--- server.js
+++ server.js
@@ -24,7 +24,15 @@ app.use(async function (ctx, next) {
 // response
 
 app.use(ctx => {
-    ctx.body = ctx.req.url;
+    const inputCommand = ctx.url.replace('/', '');
+    const bitbucketRegex = /^https:\/\/bitbucket.org\/([a-z0-9\-_]+)\/([a-z0-9\-_]+)\/pull-requests\/([0-9]+)/gi
+    const bitbucketMatch = inputCommand.match(bitbucketRegex);
+
+    if (bitbucketMatch) {
+        ctx.body = `It's a beautiful, bouncing, pull-request! Click <a href="${inputCommand}>here</a> to see it`;
+    } else {
+        ctx.body = 'Sorry, but this wasn\'t recongised.  Either it\'s not a Bitbucket pull request, or @owen sucks at regex';
+    }
 });
 
 const port = process.env.PORT || 3000;

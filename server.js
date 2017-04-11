22e30900f40270daa17772539619a7987c14d94a
Owen Jones <ojones@sorensonmedia.com>
2017-04-11 15:28:52 +0100

    Switching to ephemeral

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(ctx => {
 
     if (bitbucketMatch) {
         const responseObj = {
+            "type": "in_channel"
             "text": `${user} needs someone to look at a pull request.`,
             "attachments": [
                 {
